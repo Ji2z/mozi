dd14c504da63cf234df298e289f3e057b4cc59c0
yjanmin <yjanmin@naver.com>
2021-09-10 14:11:01 +0900

    [S05P21A603-51] feat : 탐색모드 목록

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
-    <h1>Search</h1>
+    <v-col class="" cols="12">
+      <h1 tabindex="0">탐색할 음료 선택</h1>
+    </v-col>
+    <List mode="search" />
   </div>
 </template>
 
 <script>
+import List from "../components/List";
+
 export default {
   name: "Search",
-
+  components: {
+    List,
+  },
   data() {
     return {};
   },
