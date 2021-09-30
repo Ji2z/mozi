57357f5c48ab2fb74c1b76e3efe7f1f939d3e06b
류희영 <dbgmldud8313@naver.com>
2021-09-30 14:46:36 +0900

    [S05P21A603-141] fix : Model load 에러 해결

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -5,7 +5,14 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   name: "Search",
+  methods: {
+    ...mapActions(["storeIsDetect"]),
+  },
+  created() {
+    this.storeIsDetect(true);
+  },
 };
 </script>
